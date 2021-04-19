fc18e08c2dfcf9257b9650a96453565db61b28e7
Natron0919 <Nathan.Reeb94@outlook.com>
2021-04-19 10:52:36 -0500

    Fix setup classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages = find_packages(),
     install_requires = requirements,
     classifiers = [
-        "Programming Language :: Python :: 3.9.1",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
     ]
 )
